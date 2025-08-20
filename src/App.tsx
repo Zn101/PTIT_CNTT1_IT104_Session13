f473447359355746bf27bc12aa3a56bc1d18e893
Nguyen Thac Hung <115692325+Zn101@users.noreply.github.com>
2025-08-20 22:27:09 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import ListCourse from "./components/Exercises01"
+import Children_Comp from "./components/Children_Comp";
+import ChildrenComponent from "./components/ChildrenComponent";
+import DetailPost from "./components/DetailPost";
+import Exercises01 from "./components/Exercises01";
+import Exercises02 from "./components/Exercises02";
+import Exercises03 from "./components/Exercises03";
+import ListPost from "./components/ListPost";
+import Parent_Comp from "./components/Parent_Comp";
+import ParentComponent from "./components/ParentComponent";
+import Theme from "./components/Theme";
+import "./TodoList.css";
+// import TodoList from "./components/TodoList.tsx";
 
 function App() {
 
   return (
     <div>
-      <ListCourse></ListCourse>
+      <Children_Comp />
+      <ChildrenComponent />
+      <DetailPost />
+      <Exercises01 />
+      <Exercises02 />
+      <Exercises03 />
+      <ListPost />
+      <Parent_Comp />
+      <ParentComponent />
+      <Theme />
     </div>
   )
 }
